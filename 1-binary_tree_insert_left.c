46663f28259e884acb3b0879ba2da1642dd2a5b6
Amandine4731 <5618@holbertonstudents.com>
2023-01-09 07:37:01 -0800

    valide task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,6 +9,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_parent;
 
+	if (parent == NULL)
+		return (NULL);
+
 	new_parent = malloc(sizeof(binary_tree_t));
 	if (!new_parent)
 		return (NULL);
