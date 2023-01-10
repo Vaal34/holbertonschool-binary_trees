a441cfe0debf958c59cfc7832a6990bef6e36946
Amandine4731 <5618@holbertonstudents.com>
2023-01-10 05:36:19 -0800

    valid task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: binary tree
@@ -7,13 +6,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t i = 0, j = 0; 
+	size_t i = 0, j = 0;
+
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left != NULL)
 		i += 1 + binary_tree_height(tree->left);
-	if (tree->right!= NULL)
+
+	if (tree->right != NULL)
 		j += 1 + binary_tree_height(tree->right);
 
 	if (i >= j)
