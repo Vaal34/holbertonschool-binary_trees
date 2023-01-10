677281c0703998e644c81dad1d772d9c2dce6545
Amandine4731 <5618@holbertonstudents.com>
2023-01-10 06:35:13 -0800

    test task 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_perfect - to check if a binary tree is perfect
+ * @tree: pointer to binary tree
+ * Return: 0 or the difference
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t i = 0, j = 0;
+
+	if (tree == NULL)
+		return (0);
+
+
+	if (tree->left)
+	{
+		i += 1 + binary_tree_is_perfect(tree->left);
+	}
+	if (tree->right)
+	{
+		j += 1 + binary_tree_is_perfect(tree->right);
+	}
+
+	if ((i > j) || (j > i))
+		return (0);
+
+	if (((tree->left) && (tree->right)) && (i == j))
+		return (1);
+	return (0);
+}
