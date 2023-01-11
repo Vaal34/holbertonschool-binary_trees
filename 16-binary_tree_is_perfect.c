407cb6b26440ced041f53b6a5b534011d9d778b8
Amandine4731 <5618@holbertonstudents.com>
2023-01-11 02:44:38 -0800

    test task 16 two functions

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,18 +1,12 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_perfect - to check if a binary tree is perfect
+ * recursive_tree - to go throught all elements of the binary tree
  * @tree: pointer to binary tree
- * Return: 0 or the difference
+ * Return: 0 or 1
  */
-int binary_tree_is_perfect(const binary_tree_t *tree)
+int recursive_tree(const binary_tree_t *tree)
 {
 	size_t i = 0, j = 0;
-
-	if (tree == NULL)
-		return (0);
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
 
 	if (tree->left)
 	{
@@ -22,11 +16,25 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		j += 1 + binary_tree_is_perfect(tree->right);
 	}
-
 	if ((i > j) || (j > i))
 		return (0);
-
 	if (((tree->left) && (tree->right)) && (i == j))
 		return (1);
 	return (0);
+}
+
+/**
+ * binary_tree_is_perfect - to check if a binary tree is perfect
+ * @tree: pointer to binary tree
+ * Return: 0 or 1
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	
+	return (recursive_tree(tree));
 }
