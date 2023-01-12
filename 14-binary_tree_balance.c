40ad4a71db601da25b5d4ac94502873ef44e8f25
Valentin <5619@holbertonstudents.com>
2023-01-12 02:33:33 -0800

    fix task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,8 +14,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	i = binary_tree_height(tree->left) - binary_tree_height(tree->right);
-	if (tree->parent == NULL && tree->right == NULL)
-		i += 1;
 
 	return (i);
 }
